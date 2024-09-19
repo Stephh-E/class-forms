8912ed077b59c1b26a5cea1a48bd930606137950
Steph Elsley <161695703+Stephh-E@users.noreply.github.com>
2024-09-19 20:25:49 +1000

    feat: editable form fields work

--- src/components/EditablePost.jsx
+++ src/components/EditablePost.jsx
@@ -4,23 +4,57 @@ export default class EditablePost extends React.Component {
 	constructor(props){
 		super(props);
 
+        this.state = {
+            author: props.author,
+            location: props.location,
+            content: props.content
+        }
+
 	}
 
+    handleAuthorChange = (event) => {
+        this.setState({author: event.target.value});
+    }
+
+    handleLocationChange = (event) => {
+        this.setState({author: event.target.value});
+    }
+
+    handleContentChange = (event) => {
+        this.setState({author: event.target.value});
+    }
 
 	render(){
 		return(
-            <div className="editablePostContainer">
-            {/* Editable author field */}
-            <label htmlFor="authorField">Author:</label>
-            <input type="text" name="authorField" id="authorField" />
-            {/* Editable location field */}
-            <label htmlFor="locationField">Location:</label>
-            <input type="text" name="locationField" id="locationField" />
-            {/* Editable content field */}
-            <label htmlFor="contentField">Post content:</label>
-            <input type="text" name="contentField" id="contentField" />
+			<div className="editablePostContainer">
+				{/* Editable author field */}
+				<label htmlFor="authorField">Author:</label>
+				<input 
+                type="text" 
+                name="authorField" 
+                id="authorField" 
+                value={this.state.author} 
+                onChange={(event) => this.handleAuthorChange}
+                />
 
-        </div>
+				{/* Editable location field */}
+				<label htmlFor="locationField">Location:</label>
+				<input 
+                type="text" 
+                name="locationField" 
+                id="locationField" 
+                value={this.state.location} 
+                />
+
+				{/* Editable content field */}
+				<label htmlFor="contentField">Post content:</label>
+				<input 
+                type="text" 
+                name="contentField" 
+                id="contentField" 
+                value={this.state.content} 
+                />
+			</div>
 		)
 	}
 }
